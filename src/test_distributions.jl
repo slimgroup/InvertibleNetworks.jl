81aec8bd76fa9b07112b6d60ade958a1dbe49b14
philippwitte <p.witte@ymail.com>
2020-02-23 20:18:18 -0500

    add hyperbolic network

--- src/test_distributions.jl
+++ src/test_distributions.jl
@@ -29,7 +29,7 @@ Generate samples from a 2D swirl distribution. Samples S have
 dimensions `1 x 1 x 2 x batchsize`.
 """
 function sample_swirl(batchsize; noise=1f0)
-    n = sqrt.(rand(Float32, batchsize)) .* 1000f0 .* (2f0*pi) ./ 360f0
+    n = sqrt.(rand(Float32, batchsize)) .* 800f0 .* (2f0*pi) ./ 360f0
     d1x = -cos.(n).*n .+ rand(Float32, batchsize) .* noise
     d1y = sin.(n).*n .+ rand(Float32, batchsize) .* noise
     return reshape(cat(d1x', d1y'; dims=1), 1, 1, 2, batchsize)
