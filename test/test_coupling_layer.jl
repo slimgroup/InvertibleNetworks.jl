e1df2420b9f6e8ba81b15dd97605ffe08fad403f
Philipp Witte <p.witte@ymail.com>
2020-03-05 16:58:48 -0500

    fix seed

--- test/test_coupling_layer.jl
+++ test/test_coupling_layer.jl
@@ -2,8 +2,10 @@
 # Author: Philipp Witte, pwitte3@gatech.edu
 # Date: January 2020
 
-using InvertibleNetworks, LinearAlgebra, Test
+using InvertibleNetworks, LinearAlgebra, Test, Random
 
+# Random seed
+Random.seed!(11)
 
 ###################################################################################################
 # Test invertibility
