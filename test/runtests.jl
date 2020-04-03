372303e2cf3ffb397f1d8b4527f3da6c62db588c
philippwitte <p.witte@ymail.com>
2020-04-03 09:47:05 -0400

    make permutation optional + add test for basic coupling block

--- test/runtests.jl
+++ test/runtests.jl
@@ -18,6 +18,7 @@ if test_suite == "all" || test_suite == "layers"
         # Layers
         include("test_layers/test_residual_block.jl")
         include("test_layers/test_householder_convolution.jl")
+        include("test_layers/test_coupling_layer_basic.jl")
         include("test_layers/test_coupling_layer_irim.jl")
         include("test_layers/test_coupling_layer_glow.jl")
         include("test_layers/test_coupling_layer_hint.jl")
