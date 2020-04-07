7b53ff1c38e3fc2691f1fea5081f722883dfac58
philippwitte <p.witte@ymail.com>
2020-04-07 16:13:38 -0400

    fix comments and clean up example

--- examples/applications/application_conditional_hint_seismic.jl
+++ examples/applications/application_conditional_hint_seismic.jl
@@ -6,7 +6,7 @@ using LinearAlgebra, InvertibleNetworks, PyPlot, Flux, Random, Test, JLD, Statis
 import Flux.Optimise.update!
 
 # Random seed
-Random.seed!(22)
+Random.seed!(66)
 
 
 ####################################################################################################
@@ -37,117 +37,22 @@ for j=1:ntest
     X_test[:, :, :, j:j] = wavelet_squeeze(X_orig[:, :, :, ntrain+j:ntrain+j])
 end
 
-# Network parameters
+# Create network
 n_hidden = 64
 batchsize = 4
 depth = 8
-AN_X = Array{ActNorm}(undef, depth)
-AN_Y = Array{ActNorm}(undef, depth)
-L = Array{ConditionalLayerHINT}(undef, depth)
-Params = Array{Parameter}(undef, 0)
-
-# Create layers
-for j=1:depth
-
-    # Actnorm layers
-    AN_X[j] = ActNorm(n_in; logdet=true)
-    AN_Y[j] = ActNorm(n_in; logdet=true)
-    global Params = cat(Params, get_params(AN_X[j]); dims=1)
-    global Params = cat(Params, get_params(AN_Y[j]); dims=1)
-
-    # Conditional HINT layers
-    L[j] = ConditionalLayerHINT(nx, ny, n_in, n_hidden, batchsize; permute=true)
-    global Params = cat(Params, get_params(L[j]); dims=1)
-end
-
-
-####################################################################################################
-
-# Forward pass
-function forward(X, Y)
-    logdet = 0f0
-    for j=1:depth
-        X_, logdet1 = AN_X[j].forward(X)
-        Y_, logdet2 = AN_Y[j].forward(Y)
-        X, Y, logdet3 = L[j].forward(X_, Y_)
-        logdet += (logdet1 + logdet2 + logdet3)
-    end
-    return X, Y, logdet
-end
-
-# Backward pass
-function backward(ΔX, ΔY, X, Y)
-    for j=depth:-1:1
-        ΔX_, ΔY_, X_, Y_ = L[j].backward(ΔX, ΔY, X, Y)
-        ΔX, X = AN_X[j].backward(ΔX_, X_)
-        ΔY, Y = AN_Y[j].backward(ΔY_, Y_)
-    end
-    return ΔX, ΔY, X, Y
-end
-
-# Inverse pass
-function inverse(X, Y)
-    for j=depth:-1:1
-        X_, Y_ = L[j].inverse(X, Y)
-        Y = AN_Y[j].inverse(Y_)
-        X = AN_X[j].inverse(X_)
-    end
-    return X, Y
-end
-
-# Forward pass Y-lane
-function forward_Y(Y)
-    for j=1:depth
-        Y_ = AN_Y[j].forward(Y)[1]
-        Y = L[j].forward_Y(Y_)
-    end
-    return Y
-end
-
-# Inverse pass Y-lane
-function inverse_Y(Y)
-    for j=depth:-1:1
-        Y_ = L[j].inverse_Y(Y)
-        Y = AN_Y[j].inverse(Y_)
-    end
-    return Y
-end
-
-####################################################################################################
-
-# Test layers
-test_size = 10
-idx = randperm(ntrain)[1:test_size]
-X = X_train[:, :, :, idx]
-Y = X + .1f0*randn(Float32, nx, ny, n_in, test_size)
-
-# Forward-backward
-Zx, Zy, logdet = forward(X, Y)
-X_, Y_ = backward(0f0.*Zx, 0f0.*Zy, Zx, Zy)[3:4]
-@test isapprox(norm(X - X_)/norm(X), 0f0; atol=1f-5)
-@test isapprox(norm(Y - Y_)/norm(Y), 0f0; atol=1f-5)
-
-# Forward-inverse
-Zx, Zy, logdet = forward(X, Y)
-X_, Y_ = inverse(Zx, Zy)
-@test isapprox(norm(X - X_)/norm(X), 0f0; atol=1f-5)
-@test isapprox(norm(Y - Y_)/norm(Y), 0f0; atol=1f-5)
-
-# Y-lane only
-Zyy = forward_Y(Y)
-Yy = inverse_Y(Zyy)
-@test isapprox(norm(Y - Yy)/norm(Y), 0f0; atol=1f-5)
-
+CH = NetworkConditionalHINT(nx, ny, n_in, batchsize, n_hidden, depth)
+Params = get_params(CH)
 
 ####################################################################################################
 
 # Loss
-function loss(X, Y)
-    Zx, Zy, logdet = forward(X, Y)
+function loss(CH, X, Y)
+    Zx, Zy, logdet = CH.forward(X, Y)
     f = -log_likelihood(tensor_cat(Zx, Zy)) - logdet
     ΔZ = -∇log_likelihood(tensor_cat(Zx, Zy))
     ΔZx, ΔZy = tensor_split(ΔZ)
-    ΔX, ΔY = backward(ΔZx, ΔZy, Zx, Zy)[1:2]
+    ΔX, ΔY = CH.backward(ΔZx, ΔZy, Zx, Zy)[1:2]
     return f, ΔX, ΔY
 end
 
@@ -165,7 +70,7 @@ for j=1:maxiter
     X = X_train[:, :, :, idx]
     Y = X + .5f0*randn(Float32, nx, ny, n_in, batchsize)
     
-    fval[j] = loss(X, Y)[1]
+    fval[j] = loss(CH, X, Y)[1]
     mod(j, 10) == 0 && (print("Iteration: ", j, "; f = ", fval[j], "\n"))
 
     # Update params
@@ -173,7 +78,7 @@ for j=1:maxiter
         update!(opt, p.data, p.grad)
         update!(lr_decay_fn, p.data, p.grad)
     end
-    clear_grad!(Params)
+    clear_grad!(CH)
 end
 
 ####################################################################################################
@@ -184,20 +89,20 @@ test_size = 100
 idx = randperm(ntest)[1:test_size]  # draw random samples from testing data
 X = X_test[:, :, :, idx]
 Y = X + .5f0*randn(Float32, nx, ny, n_in, test_size)
-Zx_, Zy_ = forward(X, Y)[1:2]
+Zx_, Zy_ = CH.forward(X, Y)[1:2]
 
 Zx = randn(Float32, nx, ny, n_in, test_size)
 Zy = randn(Float32, nx, ny, n_in, test_size)
-X_, Y_ = inverse(Zx, Zy)
+X_, Y_ = CH.inverse(Zx, Zy)
 
 # Now select single fixed sample from all Ys
 idx = 1
 X_fixed = X[:, :, :, idx:idx]
 Y_fixed = Y[:, :, :, idx:idx]
-Zy_fixed = forward_Y(Y_fixed)
+Zy_fixed = CH.forward_Y(Y_fixed)
 
 # Draw new Zx, while keeping Zy fixed
-X_post = inverse(Zx, Zy_fixed.*ones(Float32, nx, ny, n_in, test_size))[1]
+X_post = CH.inverse(Zx, Zy_fixed.*ones(Float32, nx, ny, n_in, test_size))[1]
 
 # Unsqueeze all tensors
 X = wavelet_unsqueeze(X)
@@ -249,6 +154,6 @@ ax4 = subplot(2,4,4); imshow(X_post_mean[:, :, 1, 1], cmap="gray", aspect="auto"
 ax5 = subplot(2,4,5); imshow(Y_fixed[:, :, 1, 1], cmap="gray", aspect="auto");  title(L"Noisy data $y_i=x_i+n$ ")
 ax6 = subplot(2,4,6); imshow(X_post[:, :, 1, 4], cmap="gray", aspect="auto"); title(L"Post. sample: $x = f(zx|zy_{fix})^{-1}$")
 ax7 = subplot(2,4,7); imshow(X_post[:, :, 1, 5], cmap="gray", aspect="auto"); title(L"Post. sample: $x = f(zx|zy_{fix})^{-1}$")
-ax8 = subplot(2,4,8); imshow(X_post_std[:, :, 1,1], cmap="binary", aspect="auto", vmin=0, vmax=0.9*maximum(X_post_std)); colorbar(); 
-title("Posterior std");
+ax8 = subplot(2,4,8); imshow(X_post_std[:, :, 1,1], cmap="binary", aspect="auto", vmin=0, vmax=0.9*maximum(X_post_std)); 
+colorbar(); title("Posterior std");
 
