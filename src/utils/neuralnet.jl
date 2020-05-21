f4109a744572548ab0df81dc3d38bbd46cb60b32
Gabrio Rizzuti <grizzuti3@gatech.edu>
2020-05-20 23:14:34 -0400

    Added reverse of NetworkConditionalHINT

--- src/utils/neuralnet.jl
+++ src/utils/neuralnet.jl
@@ -1,4 +1,4 @@
-export NeuralNetLayer, InvertibleNetwork, ReverseLayer
+export NeuralNetLayer, InvertibleNetwork, ReverseLayer, ReverseNetwork
 
 # Base Layer and network types with property getters
 
@@ -61,3 +61,40 @@ function reverse(RL::ReverseLayer)
     return R.layer
 end
 
+abstract type ReverseNetwork end
+
+function Base.getproperty(obj::ReverseNetwork, sym::Symbol)
+    if sym == :forward
+        return (args...; kwargs...) -> inverse(args..., obj.network; kwargs...)
+    elseif sym == :inverse
+        return (args...; kwargs...) -> forward(args..., obj.network; kwargs...)
+    elseif sym == :backward
+        return (args...; kwargs...) -> backward_inv(args..., obj.network; kwargs...)
+    elseif sym == :inverse_Y
+        return (args...; kwargs...) -> forward_Y(args..., obj.network; kwargs...)
+    elseif sym == :forward_Y
+        return (args...; kwargs...) -> inverse_Y(args..., obj.network; kwargs...)
+    elseif sym == :network
+        return getfield(obj, sym)
+    else
+         # fallback to getfield
+        return getfield(obj.network, sym)
+    end
+end
+
+
+struct ReverseNet <: ReverseNetwork
+    network::InvertibleNetwork
+end
+
+function reverse(N::InvertibleNetwork)
+    N_rev = deepcopy(N)
+    tag_as_reversed!(N_rev, true)
+    return ReverseNet(N_rev)
+end
+
+function reverse(RN::ReverseNetwork)
+    R = deepcopy(RN)
+    tag_as_reversed!(R.network, false)
+    return R.network
+end
--- test/test_networks/test_conditional_hint_network.jl
+++ test/test_networks/test_conditional_hint_network.jl
@@ -13,39 +13,49 @@ n_hidden = 4
 batchsize = 2
 L = 2
 K = 2
-multiscale = true
+# multiscale = true
+multiscale = false
 
 if multiscale
-    CH = NetworkMultiScaleConditionalHINT(nx, ny, n_in, batchsize, n_hidden, L, K; split_scales=false, k1=3, k2=1, p1=1, p2=0)
+    CH0 = NetworkMultiScaleConditionalHINT(nx, ny, n_in, batchsize, n_hidden, L, K; split_scales=false, k1=3, k2=1, p1=1, p2=0)
 else
-    CH = NetworkConditionalHINT(nx, ny, n_in, batchsize, n_hidden, L*K; k1=3, k2=1, p1=1, p2=0)
+    CH0 = NetworkConditionalHINT(nx, ny, n_in, batchsize, n_hidden, L*K; k1=3, k2=1, p1=1, p2=0)
 end
+nets = [CH0, reverse(CH0)]
 
-###################################################################################################
-# Invertibility
+# Loop over networks and reversed counterpart
+for j = 1:length(nets)
 
-# Test layers
-test_size = 10
-X = randn(Float32, nx, ny, n_in, test_size)
-Y = X + .1f0*randn(Float32, nx, ny, n_in, test_size)
+    CH = nets[j]
 
-# Forward-backward
-Zx, Zy, logdet = CH.forward(X, Y)
-X_, Y_ = CH.backward(0f0.*Zx, 0f0.*Zy, Zx, Zy)[3:4]
-@test isapprox(norm(X - X_)/norm(X), 0f0; atol=1f-3)
-@test isapprox(norm(Y - Y_)/norm(Y), 0f0; atol=1f-3)
+    ###################################################################################################
+    # Invertibility
 
-# Forward-inverse
-Zx, Zy, logdet = CH.forward(X, Y)
-X_, Y_ = CH.inverse(Zx, Zy)
-@test isapprox(norm(X - X_)/norm(X), 0f0; atol=1f-3)
-@test isapprox(norm(Y - Y_)/norm(Y), 0f0; atol=1f-3)
+    # Test layers
+    test_size = 10
+    X = randn(Float32, nx, ny, n_in, test_size)
+    Y = X + .1f0*randn(Float32, nx, ny, n_in, test_size)
 
-# Y-lane only
-Zyy = CH.forward_Y(Y)
-Yy = CH.inverse_Y(Zyy)
-@test isapprox(norm(Y - Yy)/norm(Y), 0f0; atol=1f-3)
+    # Forward-backward
+    Zx, Zy, logdet = CH.forward(X, Y)
+    X_, Y_ = CH.backward(0f0.*Zx, 0f0.*Zy, Zx, Zy)[3:4]
+    @test isapprox(norm(X - X_)/norm(X), 0f0; atol=1f-3)
+    @test isapprox(norm(Y - Y_)/norm(Y), 0f0; atol=1f-3)
 
+    # Forward-inverse
+    Zx, Zy, logdet = CH.forward(X, Y)
+    X_, Y_ = CH.inverse(Zx, Zy)
+    @test isapprox(norm(X - X_)/norm(X), 0f0; atol=1f-3)
+    @test isapprox(norm(Y - Y_)/norm(Y), 0f0; atol=1f-3)
+
+    # Y-lane only
+    if ~CH.is_reversed
+        Zyy = CH.forward_Y(Y)
+        Yy = CH.inverse_Y(Zyy)
+        @test isapprox(norm(Y - Yy)/norm(Y), 0f0; atol=1f-3)
+    end
+
+end
 
 ###################################################################################################
 # Gradient test
@@ -62,32 +72,41 @@ end
 
 # Gradient test w.r.t. input
 if multiscale
-    CH = NetworkMultiScaleConditionalHINT(nx, ny, n_in, batchsize, n_hidden, L, K; split_scales=false, k1=3, k2=1, p1=1, p2=0)
+    CH0 = NetworkMultiScaleConditionalHINT(nx, ny, n_in, batchsize, n_hidden, L, K; split_scales=false, k1=3, k2=1, p1=1, p2=0)
 else
-    CH = NetworkConditionalHINT(nx, ny, n_in, batchsize, n_hidden, L*K; k1=3, k2=1, p1=1, p2=0)
+    CH0 = NetworkConditionalHINT(nx, ny, n_in, batchsize, n_hidden, L*K; k1=3, k2=1, p1=1, p2=0)
 end
+nets = [CH0, reverse(CH0)]
 
-X = randn(Float32, nx, ny, n_in, test_size)
-Y = X + .1f0*randn(Float32, nx, ny, n_in, test_size)
-X0 = randn(Float32, nx, ny, n_in, test_size)
-Y0 = X0 + .1f0*randn(Float32, nx, ny, n_in, test_size)
-dX = X - X0
-dY = Y - Y0
+# Loop over networks and reversed counterpart
+for j = 1:length(nets)
 
-f0, ΔX, ΔY = loss(CH, X0, Y0)
-h = 0.1f0
-maxiter = 6
-err1 = zeros(Float32, maxiter)
-err2 = zeros(Float32, maxiter)
+    CH = nets[j]
 
-print("\nGradient test cond. HINT net: input\n")
-for j=1:maxiter
-    f = loss(CH, X0 + h*dX, Y0 + h*dY)[1]
-    err1[j] = abs(f - f0)
-    err2[j] = abs(f - f0 - h*dot(dX, ΔX) - h*dot(dY, ΔY))
-    print(err1[j], "; ", err2[j], "\n")
-    global h = h/2f0
+    test_size = 10
+    X = randn(Float32, nx, ny, n_in, test_size)
+    Y = X + .1f0*randn(Float32, nx, ny, n_in, test_size)
+    X0 = randn(Float32, nx, ny, n_in, test_size)
+    Y0 = X0 + .1f0*randn(Float32, nx, ny, n_in, test_size)
+    dX = X - X0
+    dY = Y - Y0
+
+    f0, ΔX, ΔY = loss(CH, X0, Y0)
+    h = 0.1f0
+    maxiter = 6
+    err1 = zeros(Float32, maxiter)
+    err2 = zeros(Float32, maxiter)
+
+    print("\nGradient test cond. HINT net: input\n")
+    for j=1:maxiter
+        f = loss(CH, X0 + h*dX, Y0 + h*dY)[1]
+        err1[j] = abs(f - f0)
+        err2[j] = abs(f - f0 - h*dot(dX, ΔX) - h*dot(dY, ΔY))
+        print(err1[j], "; ", err2[j], "\n")
+        h = h/2f0
+    end
+
+    @test isapprox(err1[end] / (err1[1]/2^(maxiter-1)), 1f0; atol=1f1)
+    @test isapprox(err2[end] / (err2[1]/4^(maxiter-1)), 1f0; atol=1f1)
+
 end
-
-@test isapprox(err1[end] / (err1[1]/2^(maxiter-1)), 1f0; atol=1f1)
-@test isapprox(err2[end] / (err2[1]/4^(maxiter-1)), 1f0; atol=1f1)
