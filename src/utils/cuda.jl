9f209d1b8a127c7f3b6a3dfc6e93f1c279f0f0b9
Ali Siahkoohi <alisk@gatech.edu>
2020-08-20 18:28:07 -0400

    Cuarrays not defnined

--- src/utils/cuda.jl
+++ src/utils/cuda.jl
@@ -4,5 +4,5 @@ export convert_cu, cuzeros, cuones
 
 convert_cu(in_a, X) =  X isa CuArray ? cu(in_a) : in_a
 
-cuzeros(X, args...) = X isa CuArray ? convert_cu(zeros(Float32, args)) : zeros(Float32, args)
-cuones(X, args...) = X isa CuArray ? convert_cu(ones(Float32, args)) : ones(Float32, args)
+cuzeros(X, args...) = X isa CuArray ? convert_cu(zeros(Float32, args), X) : zeros(Float32, args)
+cuones(X, args...) = X isa CuArray ? convert_cu(ones(Float32, args), X) : ones(Float32, args)
