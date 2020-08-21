767620a9703237b9ca36dc71c3842b5c374f0e4a
Ali Siahkoohi <alisk@gatech.edu>
2020-08-21 12:53:51 -0400

    edit cuda.jl

--- src/utils/cuda.jl
+++ src/utils/cuda.jl
@@ -3,5 +3,5 @@ using CUDA
 export convert_cu, cuzeros, cuones
 
 convert_cu(in_a, X) =  X isa CuArray ? cu(in_a) : in_a
-cuzeros(X, args...) = X isa CuArray ? CUDA.zeros(args), args) : zeros(Float32, args)
-cuones(X, args...) = X isa CuArray ? CUDA.ones(args), args) : ones(Float32, args)
+cuzeros(X, args...) = X isa CuArray ? CUDA.zeros(args) : zeros(Float32, args)
+cuones(X, args...) = X isa CuArray ? CUDA.ones(args) : ones(Float32, args)
