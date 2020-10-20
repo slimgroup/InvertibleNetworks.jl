057bd1878fec2656c76a5326cb7a72172eb0fc49
pwitte <pwitte@microsoft.com>
2020-10-20 16:17:04 -0700

    decrease image size

--- test/test_utils/test_squeeze.jl
+++ test/test_utils/test_squeeze.jl
@@ -30,7 +30,7 @@ b = dot(X, wavelet_unsqueeze(Y))
 
 ##############################################################################################################################
 # 5D Tensor
-X = randn(Float32, 28, 28, 28, 2, 4)
+X = randn(Float32, 16, 16, 16, 2, 4)
 
 # Squeeze and unsqueeze
 @test isapprox(norm(X - unsqueeze(squeeze(X; pattern="column"); pattern="column")), 0f0; atol=1f-6)
@@ -42,8 +42,8 @@ X_ = wavelet_unsqueeze(Y)
 @test isapprox(norm(X - X_)/norm(X), 0f0; atol=1f-5)
 
 # Wavelet transform adjoint test
-X = randn(Float32, 28, 28, 28, 2, 4)
-Y = randn(Float32, 14, 14, 14, 16, 4)
+X = randn(Float32, 16, 16, 16, 2, 4)
+Y = randn(Float32, 8, 8, 8, 16, 4)
 a = dot(Y, wavelet_squeeze(X))
 b = dot(X, wavelet_unsqueeze(Y))
 @test isapprox(a/b - 1f0, 0f0; atol=1f-5)
