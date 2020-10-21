24f16be4b2244d5a2c62835143d43b965ac16f15
Gabrio Rizzuti <rizzuti.gabrio@gmail.com>
2020-10-20 21:44:09 -0400

    Added extra activations from Ali branch

--- test/test_utils/test_activations.jl
+++ test/test_utils/test_activations.jl
@@ -185,3 +185,93 @@ end
 @test isapprox(err1[end] / (err1[1]/2^(maxiter-1)), 1f0; atol=1f1)
 @test isapprox(err2[end] / (err2[1]/4^(maxiter-1)), 1f0; atol=1f1)
 
+
+###############################################################################
+# Sigmoid2 (scaled version of Sigmoid)
+
+nx = 12
+ny = 12
+n_in = 4
+batchsize = 2
+
+X = randn(Float32, nx, ny, n_in, batchsize)
+X0 = randn(Float32, nx, ny, n_in, batchsize)
+dX = X - X0
+
+# Gradient test Sigmoid2
+L = Sigmoid2Layer()
+function objective(X, Y)
+    Y0 = L.forward(X)
+    ΔY = Y0 - Y
+    f = .5f0*norm(ΔY)^2
+    ΔX = L.backward(ΔY, Y0)
+    return f, ΔX
+end
+
+# Observed data
+Y = L.forward(X)
+
+# Gradient test for X
+maxiter = 5
+print("\nGradient test Sigmoid2\n")
+f0, ΔX = objective(X0, Y)
+h = .1f0
+err1 = zeros(Float32, maxiter)
+err2 = zeros(Float32, maxiter)
+for j=1:maxiter
+    f = objective(X0 + h*dX, Y)[1]
+    err1[j] = abs(f - f0)
+    err2[j] = abs(f - f0 - h*dot(dX, ΔX))
+    print(err1[j], "; ", err2[j], "\n")
+    global h = h/2f0
+end
+
+@test isapprox(err1[end] / (err1[1]/2^(maxiter-1)), 1f0; atol=1f1)
+@test isapprox(err2[end] / (err2[1]/4^(maxiter-1)), 1f0; atol=1f1)
+
+
+###############################################################################
+# ExpClamp
+
+nx = 12
+ny = 12
+n_in = 4
+batchsize = 3
+
+X = glorot_uniform(nx, ny, n_in, batchsize)
+X0 = glorot_uniform(nx, ny, n_in, batchsize)
+dX = X - X0
+
+# Invertibility
+err = norm(X - ExpClampInv(ExpClamp(X))) / norm(X)
+@test isapprox(err, 0f0, atol=1f-5)
+
+# Gradient test sigmoid
+function objective(X, Y)
+    Y0 = ExpClamp(X)
+    ΔY = Y0 - Y
+    f = .5f0*norm(ΔY)^2
+    ΔX = ExpClampGrad(ΔY, Y0)
+    return f, ΔX
+end
+
+# Observed data
+Y = ExpClamp(X)
+
+# Gradient test for X
+maxiter = 5
+print("\nGradient test ExpClamp\n")
+f0, ΔX = objective(X0, Y)
+h = .1f0
+err1 = zeros(Float32, maxiter)
+err2 = zeros(Float32, maxiter)
+for j=1:maxiter
+    f = objective(X0 + h*dX, Y)[1]
+    err1[j] = abs(f - f0)
+    err2[j] = abs(f - f0 - h*dot(dX, ΔX))
+    print(err1[j], "; ", err2[j], "\n")
+    global h = h/2f0
+end
+
+@test isapprox(err1[end] / (err1[1]/2^(maxiter-1)), 1f0; atol=1f1)
+@test isapprox(err2[end] / (err2[1]/4^(maxiter-1)), 1f0; atol=1f1)
