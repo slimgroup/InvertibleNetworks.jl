5d39da781f415dc20ee23ae48e1a490205b41154
Gabrio Rizzuti <rizzuti.gabrio@gmail.com>
2020-10-29 16:56:01 -0400

    Fixed Jacobian abstraction

--- src/utils/jacobian.jl
+++ src/utils/jacobian.jl
@@ -4,104 +4,96 @@
 #
 # Jacobian utilities
 
-export Jacobian, Jacobian!
+export Jacobian
 
-function Jacobian(Net::Union{InvertibleNetwork,NeuralNetLayer}, X::Array{Float32,N}; Y::Union{Nothing,Array{Float32,N}}=nothing, opt_output=false) where N
 
-    # Domain and range data types
-    DDT = Tuple{Array{Float32,N}, Array{Parameter,1}}
-    RDT = Array{Float32,N}
+## Jacobian types
 
-    # Clean output option
-    if !opt_output
+struct JacobianInvertibleNetwork <: joAbstractLinearOperator{Float32, Float32}
+    name::String
+    n::Int64
+    m::Int64
+    fop::Function
+    fop_T::Function
+    N::Union{NeuralNetLayer, InvertibleNetwork}
+    X::Array{Float32}
+    Y::Union{Nothing, Array{Float32}}
+end
 
-        # Forward evaluation
-        fop1(input::Tuple{Array{Float32,N}, Array{Parameter,1}}) = Net.jacobian(input[1], input[2], X)[1]
-        fop1(Δθ::Array{Parameter,1}) = Net.jacobian(zeros(Float32, size(X)), Δθ, X)[1]
+struct JacobianInvertibleNetworkAdjoint <: joAbstractLinearOperator{Float32, Float32}
+    name::String
+    n::Int64
+    m::Int64
+    fop::Function
+    fop_T::Function
+    N::Union{NeuralNetLayer, InvertibleNetwork}
+    X::Array{Float32}
+    Y::Union{Nothing, Array{Float32}}
+end
 
-        # Adjoint evaluation
-        if isnothing(Y) # recomputing Y=f(X) if not provided
-            Y = Net.forward(X)
-            isa(Y, Tuple) && (Y = Y[1])
-        end
-        fop1_adj(ΔY::Array{Float32,N}) = Net.adjointJacobian(ΔY, Y)[1:2]
 
-        return InvertibleNetworkLinearOperator{RDT,DDT}(fop1, fop1_adj)
+## Constructor
 
-    else
+function Jacobian(N::Union{InvertibleNetwork, NeuralNetLayer}, X::Array{Float32}; Y::Union{Nothing, Array{Float32}}=nothing, save_Y::Bool=true, io_mode::String="θ↦Y", name::String="Jacobian")
 
-        # Forward evaluation
-        fop(input::Tuple{Array{Float32,N}, Array{Parameter,1}}) = Net.jacobian(input[1], input[2], X)
-        fop(Δθ::Array{Parameter,1}) = Net.jacobian(zeros(Float32, size(X)), Δθ, X)
-        fop(input::Tuple{Array{Float32,N}, Array{Parameter,1}, Array{Float32,N}, Array{Parameter,1}}) = Net.jacobian(input[1], input[2], X) # when input = full output from adjoint eval (logdet=true)
-        fop(input::Tuple{Array{Float32,N}, Array{Parameter,1}, Array{Float32,N}}) = Net.jacobian(input[1], input[2], X) # when input = full output from adjoint eval (logdet=false)
-
-        # Adjoint evaluation
-        if isnothing(Y) # recomputing Y=f(X) if not provided
-            Y = Net.forward(X)
-            isa(Y, Tuple) && (Y = Y[1])
-        end
-        fop_adj(ΔY::Array{Float32,N}) = Net.adjointJacobian(ΔY, Y)
-        fop_adj(input::Tuple{Array{Float32,N}, Array{Float32,N}, Float32, Array{Parameter,1}}) = Net.adjointJacobian(input[1], input[2]) # when input = full output from forward eval (logdet=true)
-        fop_adj(input::Tuple{Array{Float32,N}, Array{Float32,N}}) = Net.adjointJacobian(input[1], input[2]) # when input = full output from forward eval (logdet=true)
-
-        return InvertibleNetworkLinearOperator{RDT,DDT}(fop, fop_adj)
-
+    # Computing & storing Y=f(X) if requested
+    if save_Y && isnothing(Y)
+        Y = N.forward(X)
+        isa(Y, Tuple) && (Y = Y[1])
     end
+
+    # Input/output dimensions
+    m = length(X)
+    n = length(par2vec(get_params(N))[1])
+    ((io_mode == "X×θ↦Y") || (io_mode == "X×θ↦Y")) && (n += m)
+
+    # Forward evaluation
+    function fop(ΔX::Union{Nothing, Array{Float32}}, Δθ::Array{Parameter, 1})
+        isnothing(ΔX) && (ΔX = cuzeros(X, size(X)...))
+        out = N.jacobian(ΔX, Δθ, X)
+        ((io_mode == "θ↦Y") || (io_mode == "X×θ↦Y")) && (return out[1])
+        io_mode == "full"                            && (return out)
+    end
+
+    # Adjoint evaluation
+    function fop_adj(ΔY::Array{Float32}; Y::Union{Nothing, Array{Float32}}=Y)
+        if isnothing(Y)
+            Y = N.forward(X)
+            isa(Y, Tuple) && (Y = Y[1])
+        end
+        out = N.adjointJacobian(ΔY, Y)
+        io_mode == "θ↦Y"   && (return out[2])
+        io_mode == "X×θ↦Y" && (return out[1:2])
+        io_mode == "full"  && (return out)
+    end
+
+    # Jacobian JOLI
+    joJ = joLinearFunctionFwd_T(n, m, fop, fop_adj, Float32, Float32; name=name)
+
+    # Output
+    return JacobianInvertibleNetwork(name, n, m, fop, fop_adj, N, X, Y)
 
 end
 
-function Jacobian!(Net::Union{InvertibleNetwork,NeuralNetLayer}, X::Array{Float32,N}; θ::Union{Nothing,Array{Parameter,1}}=nothing, Y::Union{Nothing,Array{Float32,N}}=nothing, opt_output=false) where N
 
-    # Set parameters if provided
-    if !isnothing(θ)
-        set_params!(Net, θ)
-    end
+## Algebra
 
-    # Domain and range data types
-    DDT = Tuple{Array{Float32,N}, Array{Parameter,1}}
-    RDT = Array{Float32,N}
-
-    # Clean output option
-    if !opt_output
-
-        # Forward evaluation
-        fop1(input::Tuple{Array{Float32,N}, Array{Parameter,1}}) = Net.jacobian(input[1], input[2], X)[1]
-        fop1(Δθ::Array{Parameter,1}) = Net.jacobian(zeros(Float32, size(X)), Δθ, X)[1]
-
-        # Adjoint evaluation
-        if isnothing(Y) # recomputing Y=f(X) if not provided
-            Y = Net.forward(X)
-            isa(Y, Tuple) && (Y = Y[1])
-        end
-        fop1_adj(ΔY::Array{Float32,N}) = Net.adjointJacobian(ΔY, Y)[1:2]
-
-        return InvertibleNetworkLinearOperator{RDT,DDT}(fop1, fop1_adj)
-
-    else
-
-        # Forward evaluation
-        fop(input::Tuple{Array{Float32,N}, Array{Parameter,1}}) = Net.jacobian(input[1], input[2], X)
-        fop(Δθ::Array{Parameter,1}) = Net.jacobian(zeros(Float32, size(X)), Δθ, X)
-        fop(input::Tuple{Array{Float32,N}, Array{Parameter,1}, Array{Float32,N}, Array{Parameter,1}}) = Net.jacobian(input[1], input[2], X) # when input = full output from adjoint eval (logdet=true)
-        fop(input::Tuple{Array{Float32,N}, Array{Parameter,1}, Array{Float32,N}}) = Net.jacobian(input[1], input[2], X) # when input = full output from adjoint eval (logdet=false)
-
-        # Adjoint evaluation
-        if isnothing(Y) # recomputing Y=f(X) if not provided
-            Y = Net.forward(X)
-            isa(Y, Tuple) && (Y = Y[1])
-        end
-        fop_adj(ΔY::Array{Float32,N}) = Net.adjointJacobian(ΔY, Y)
-        fop_adj(input::Tuple{Array{Float32,N}, Array{Float32,N}, Float32, Array{Parameter,1}}) = Net.adjointJacobian(input[1], input[2]) # when input = full output from forward eval (logdet=true)
-        fop_adj(input::Tuple{Array{Float32,N}, Array{Float32,N}}) = Net.adjointJacobian(input[1], input[2]) # when input = full output from forward eval (logdet=true)
-
-        return InvertibleNetworkLinearOperator{RDT,DDT}(fop, fop_adj)
-
-    end
-
+function adjoint(J::JacobianInvertibleNetwork)
+    return JacobianInvertibleNetworkAdjoint(string("adjoint(", J.name, ")"), J.m, J.n, J.fop_T, J.fop, J.N, J.X, J.Y)
 end
 
-# Special input case
-function *(J::InvertibleNetworkLinearOperator{Array{Float32,N},Tuple{Array{Float32,N}, Array{Parameter,1}}}, Δθ::Array{Parameter,1}) where N
-    return J.fop(Δθ)
+function adjoint(JT::JacobianInvertibleNetworkAdjoint)
+    return JacobianInvertibleNetwork(JT.name[9:end-1], JT.m, JT.n, JT.fop_T, JT.fop, JT.N, JT.X, JT.Y)
+end
+
+function *(J::JacobianInvertibleNetwork, Δθ::Array{Parameter,1})
+    return J.fop(nothing, Δθ)
+end
+
+function *(J::JacobianInvertibleNetwork, input::Tuple{Array{Float32}, Array{Parameter,1}})
+    return J.fop(input[1], input[2])
+end
+
+function *(JT::JacobianInvertibleNetworkAdjoint, ΔY::Array{Float32})
+    return JT.fop(ΔY)
 end
