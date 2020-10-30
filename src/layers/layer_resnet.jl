a6edbeef91d1e62e0727a3237bb956cbac7ad44c
Gabrio Rizzuti <rizzuti.gabrio@gmail.com>
2020-10-29 22:34:58 -0400

    Fixed resnet + example

--- src/layers/layer_resnet.jl
+++ src/layers/layer_resnet.jl
@@ -5,19 +5,37 @@
 export ResNet
 
 
-function ResNet(n_in::Int64, n_hidden::Int64, nblocks::Int64; k::Int64=3, p::Int64=1, s::Int64=1, norm=BatchNorm, n_out::Union{Nothing, Int64}=nothing)
+function ResNet(n_in::Int64, n_hidden::Int64, nblocks::Int64; k::Int64=3, p::Int64=1, s::Int64=1, norm::Union{Nothing, String}="batch", n_out::Union{Nothing, Int64}=nothing)
 
-    resnet_blocks = Array{Any, 1}(undef, nblocks+!isnothing(n_out))
-    for i = 1:nblocks
-        if isnothing(n_out)
-            resnet_blocks[i] = SkipConnection(Chain(Conv((k, k), n_in => n_hidden; stride = s, pad = p), Conv((k, k), n_hidden => n_in; stride = s, pad = p)), +)
-        else
-            resnet_blocks[i] = SkipConnection(Chain(Conv((k, k), n_in => n_hidden; stride = s, pad = p), norm(n_hidden, relu), Conv((k, k), n_hidden => n_in; stride = s, pad = p)), +)
-        end
+    resnet_blocks = Array{Any, 1}(undef, nblocks)
+    for i = 1:nblocks-1
+
+        # Normalization layer
+        (norm == "batch")  && (NormLayer = BatchNorm(n_hidden))
+        (norm === nothing) && (NormLayer = identity)
+
+        # Skip-connection
+        resnet_blocks[i]   = SkipConnection(Chain(Conv((k, k), n_in => n_hidden; stride = s, pad = p), NormLayer, x->relu.(x), Conv((k, k), n_hidden => n_in; stride = s, pad = p)), +)
+
     end
-    !isnothing(n_out) && (resnet_blocks[end] = Conv((k, k), n_in => n_out; stride = s, pad = p))
-    model = Chain(resnet_blocks...)
 
-    return FluxBlock(model)
+    # Last layer
+    if isnothing(n_out)
+
+        # Normalization layer
+        (norm == "batch")  && (NormLayer = BatchNorm(n_hidden))
+        (norm === nothing) && (NormLayer = identity)
+
+        # Skip-connection
+        resnet_blocks[end] = SkipConnection(Chain(Conv((k, k), n_in => n_hidden; stride = s, pad = p), NormLayer, x->relu.(x),Conv((k, k), n_hidden => n_in; stride = s, pad = p)), +)
+
+    else
+
+        # Simple convolution
+        resnet_blocks[end] = Conv((k, k), n_in => n_out; stride = s, pad = p)
+
+    end
+
+    return FluxBlock(Chain(resnet_blocks...))
 
 end
