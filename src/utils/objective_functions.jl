0b3be5401f1b37efa1714d278e3c085d9292b489
Ali Siahkoohi <ali.siahkoohi@gmail.com>
2021-02-18 12:13:15 -0500

    suggested fixes to address gpu bugs

--- src/utils/objective_functions.jl
+++ src/utils/objective_functions.jl
@@ -13,7 +13,7 @@ Mean squared error between arrays/tensor X and Y
 
 See also: [`∇mse`](@ref)
 """
-mse(X::Array{Float32, N}, Y::Array{Float32, N}) where N = .5f0/size(X, N)*norm(X - Y, 2)^2
+mse(X::AbstractArray{Float32, N}, Y::AbstractArray{Float32, N}) where N = .5f0/size(X, N)*norm(X - Y, 2)^2
 
 
 """
@@ -50,7 +50,7 @@ Log-likelihood of X for a Gaussian distribution with given mean μ and variance 
 
 See also: [`∇log_likelihood`](@ref)
 """
-log_likelihood(X::Array{Float32, N}; μ=0f0, σ=1f0) where N = 1f0/size(X, N)*sum(-.5f0*((X .- μ)/σ).^2)
+log_likelihood(X::AbstractArray{Float32, N}; μ=0f0, σ=1f0) where N = 1f0/size(X, N)*sum(-.5f0*((X .- μ)/σ).^2)
 
 
 """
@@ -60,7 +60,7 @@ Hessian of the log-likelihood function with respect to the input tensor X.
 
 See also: [`log_likelihood`](@ref)
 """
-∇log_likelihood(X::Array{Float32, N}; μ=0f0, σ=1f0) where N = -1f0/size(X, N)*(X .- μ)/σ^2
+∇log_likelihood(X::AbstractArray{Float32, N}; μ=0f0, σ=1f0) where N = -1f0/size(X, N)*(X .- μ)/σ^2
 
 
 """
@@ -70,8 +70,8 @@ Hessian of the log-likelihood function with respect to the input tensor X.
 
 See also: [`log_likelihood`](@ref)
 """
-function Hlog_likelihood(X::Array{Float32, N}; μ=0f0, σ=1f0) where N
-    return InvertibleNetworkLinearOperator{Array{Float32, N},Array{Float32, N}}(
+function Hlog_likelihood(X::AbstractArray{Float32, N}; μ=0f0, σ=1f0) where N
+    return InvertibleNetworkLinearOperator{AbstractArray{Float32, N},AbstractArray{Float32, N}}(
         ΔX -> -1f0/size(X, N)*ΔX/σ^2,
         ΔX -> -1f0/size(X, N)*ΔX/σ^2)
 end
