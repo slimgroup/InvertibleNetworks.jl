6771880e3a8f17ece3df7ff616cfffbd3cab6dfd
rafaelorozco <rorozcom3>
2021-03-23 22:35:20 -0400

    Adding squeeze type functionality and tests

--- test/test_networks/test_multiscale_conditional_hint_network.jl
+++ test/test_networks/test_multiscale_conditional_hint_network.jl
@@ -14,36 +14,36 @@ batchsize = 2
 L = 2
 K = 2
 
-# Multi-scale network with and without logdet
-CH0_multi = NetworkMultiScaleConditionalHINT(n_in, n_hidden, L, K; split_scales=false, k1=3, k2=1, p1=1, p2=0)
-CH1_multi = NetworkMultiScaleConditionalHINT(n_in, n_hidden, L, K; logdet=false, split_scales=false, k1=3, k2=1, p1=1, p2=0)
+function inv_test(nx, ny, n_in, batchsize, logdet, squeeze_type, split_scales)
+    print("\nMultiscale Conditional HINT invertibility test with squeeze_type=$(squeeze_type), split_scales=$(split_scales), logdet=$(logdet)\n")
+    CH = NetworkMultiScaleConditionalHINT(n_in, n_hidden, L, K; squeeze_type = squeeze_type, logdet=logdet, split_scales=split_scales)
 
-nets_multi = [CH0_multi, CH1_multi, reverse(CH0_multi), reverse(CH1_multi)] 
+    # Input image and data
+    X = randn(Float32, nx, ny, n_in, batchsize)
+    Y = randn(Float32, nx, ny, n_in, batchsize)
 
-function test_inv(CH, nx, ny, n_in)
-    print("\nInvertibility test HINT network\n")
-    # Test layers
-    test_size = 10
-    X = randn(Float32, nx, ny, n_in, test_size)
-    Y = X + .1f0*randn(Float32, nx, ny, n_in, test_size)
 
-    # Forward-backward
-    Zx, Zy = CH.forward(X, Y)[1:2]
-    X_, Y_ = CH.backward(0f0.*Zx, 0f0.*Zy, Zx, Zy)[3:4]
-    @test isapprox(norm(X - X_)/norm(X), 0f0; atol=1f-3)
-    @test isapprox(norm(Y - Y_)/norm(Y), 0f0; atol=1f-3)
-
-    # Forward-inverse
+    # Test inverse
     Zx, Zy = CH.forward(X, Y)[1:2]
     X_, Y_ = CH.inverse(Zx, Zy)
+
     @test isapprox(norm(X - X_)/norm(X), 0f0; atol=1f-3)
     @test isapprox(norm(Y - Y_)/norm(Y), 0f0; atol=1f-3)
 
-    # Y-lane only
-    Zyy = CH.forward_Y(Y)
-    Yy = CH.inverse_Y(Zyy)
-    @test isapprox(norm(Y - Yy)/norm(Y), 0f0; atol=1f-3)
-end
+    # Test backward
+    ΔZx = randn(Float32, size(Zx))  # random derivative
+    ΔZy = randn(Float32, size(Zx))
+    ΔX_, ΔY_, X_, Y_ = CH.backward(ΔZx, ΔZy, Zx, Zy)
+
+    @test isapprox(norm(X - X_)/norm(X), 0f0; atol=1f-3)
+    @test isapprox(norm(Y - Y_)/norm(Y), 0f0; atol=1f-3)
+
+    # Test inverse Y only
+    Zy = CH.forward_Y(Y)
+    Y_ = CH.inverse_Y(Zy)
+
+    @test isapprox(norm(Y - Y_)/norm(Y), 0f0; atol=1f-3)
+end   
 
 # Loss
 function loss(CH, X, Y)
@@ -60,26 +60,32 @@ function loss(CH, X, Y)
     return f, ΔX, ΔY
 end
 
-function test_grad(CH, nx, ny, n_in)
-    test_size = 10
-    X = randn(Float32, nx, ny, n_in, test_size)
-    Y = X + .1f0*randn(Float32, nx, ny, n_in, test_size)
-    X0 = randn(Float32, nx, ny, n_in, test_size)
-    Y0 = X0 + .1f0*randn(Float32, nx, ny, n_in, test_size)
-    dX = X - X0
-    dY = Y - Y0
 
-    f0, ΔX, ΔY = loss(CH, X0, Y0)
+
+function grad_test_X(nx, ny, n_channel, batchsize, logdet, squeeze_type, split_scales)
+    #logdet ? lossf = loss_logdet : lossf = loss
+    print("\nMultiscale Conditional HINT invertibility test with squeeze_type=$(squeeze_type), split_scales=$(split_scales), logdet=$(logdet)\n")
+    CH = NetworkMultiScaleConditionalHINT(n_in, n_hidden, L, K; squeeze_type = squeeze_type, logdet=logdet, split_scales=split_scales)
+
+    # Input image
+    X0 = randn(Float32, nx, ny, n_channel, batchsize)
+    dX = randn(Float32, nx, ny, n_channel, batchsize)
+
+    # Input data
+    Y0 = randn(Float32, nx, ny, n_channel, batchsize)
+    dY = randn(Float32, nx, ny, n_channel, batchsize)
+
+    f0, gX, gY = loss(CH, X0, Y0)[1:3]
+
+    maxiter = 5
     h = 0.1f0
-    maxiter = 6
     err1 = zeros(Float32, maxiter)
     err2 = zeros(Float32, maxiter)
 
-    print("\nGradient test cond. HINT net: input\n")
     for j=1:maxiter
         f = loss(CH, X0 + h*dX, Y0 + h*dY)[1]
         err1[j] = abs(f - f0)
-        err2[j] = abs(f - f0 - h*dot(dX, ΔX) - h*dot(dY, ΔY))
+        err2[j] = abs(f - f0 - h*dot(dX, gX) - h*dot(dY, gY))
         print(err1[j], "; ", err2[j], "\n")
         h = h/2f0
     end
@@ -88,58 +94,50 @@ function test_grad(CH, nx, ny, n_in)
     @test isapprox(err2[end] / (err2[1]/4^(maxiter-1)), 1f0; atol=1f1)
 end
 
-# Loop over networks and reversed counterpart
-for CH in nets_multi
-	#add logic to change input dimensions since wavelet transforms change them.
-    CH.is_reversed ? test_inv(CH, nx÷(2^CH.L), ny ÷ (2^CH.L), n_in*(4^CH.L)) : test_inv(CH, nx, ny, n_in)
-    CH.is_reversed ? test_grad(CH, nx÷(2^CH.L), ny ÷ (2^CH.L), n_in*(4^CH.L)) : test_grad(CH, nx, ny, n_in)
+function grad_test_par(nx, ny, n_channel, batchsize, logdet, squeeze_type, split_scales)
+    print("\nMultiscale Conditional HINT invertibility test with squeeze_type=$(squeeze_type), split_scales=$(split_scales), logdet=$(logdet)\n")
+    #logdet ? lossf = loss_logdet : lossf = loss
+    CH = NetworkMultiScaleConditionalHINT(n_in, n_hidden, L, K; squeeze_type = squeeze_type, logdet=logdet, split_scales=split_scales)
+
+
+    CHini = deepcopy(CH0)
+
+    # Perturbation
+    X = randn(Float32, nx, ny, n_channel, batchsize)
+    Y = randn(Float32, nx, ny, n_channel, batchsize)
+    dW = randn(Float32, size(CH0.CL_X.CL[1].RB.W1))
+
+    f0, gW, gv = loss(CH0, X, Y)[[1,4,5]]
+
+    maxiter = 5
+    h = 0.1f0
+
+    err3 = zeros(Float32, maxiter)
+    err4 = zeros(Float32, maxiter)
+
+    for j=1:maxiter
+        CH0.CL_X.CL[1].RB.W1.data = CHini.CL_X.CL[1].RB.W1.data + h*dW
+        f = loss(CH0, X, Y)[1]
+        err3[j] = abs(f - f0)
+        err4[j] = abs(f - f0 - h*dot(gW, dW))
+        print(err3[j], "; ", err4[j], "\n")
+        h = h/2f0
+    end
+
+    @test isapprox(err3[end] / (err3[1]/2^(maxiter-1)), 1f0; atol=1f1)
+    @test isapprox(err4[end] / (err4[1]/4^(maxiter-1)), 1f0; atol=1f1)
+end
+
+for squeeze_type in ["normal", "wavelet", "Haar"]
+    for split_scales in [true, false]
+        for logdet in [true, false]
+            inv_test(nx, ny, n_in, batchsize, logdet, squeeze_type, split_scales)
+            grad_test_X(nx, ny, n_in, batchsize, logdet, squeeze_type, split_scales)
+            #grad_test_par(nx, ny, n_in, batchsize, logdet, squeeze_type, split_scales)    
+        end
+    end
 end
 
 ###################################################################################################
-# Jacobian-related tests: NetworkMultiScaleConditionalHINT
-
-# Gradient test
-
-# Initialization
-CH = NetworkMultiScaleConditionalHINT(n_in, n_hidden, L, K; split_scales=false, k1=3, k2=1, p1=1, p2=0); CH.forward(randn(Float32, nx, ny, n_in, batchsize), randn(Float32, nx, ny, n_in, batchsize))
-θ = deepcopy(get_params(CH))
-CH0 = NetworkMultiScaleConditionalHINT(n_in, n_hidden, L, K; split_scales=false, k1=3, k2=1, p1=1, p2=0); CH0.forward(randn(Float32, nx, ny, n_in, batchsize), randn(Float32, nx, ny, n_in, batchsize))
-θ0 = deepcopy(get_params(CH0))
-X = randn(Float32, nx, ny, n_in, batchsize)
-Y = randn(Float32, nx, ny, n_in, batchsize)
-
-# Perturbation (normalized)
-dθ = θ-θ0; dθ .*= (norm.(θ).+1f-6)./(norm.(dθ).+1f-6)
-dX = randn(Float32, nx, ny, n_in, batchsize); dX *= norm(X)/norm(dX)
-dY = randn(Float32, nx, ny, n_in, batchsize); dY *= norm(Y)/norm(dY)
-
-# Jacobian eval
-dZx, dZy, Zx, Zy, _, _ = CH.jacobian(dX, dY, dθ, X, Y)
-
-# Test
-print("\nJacobian test\n")
-h = 0.1f0
-maxiter = 5
-err5 = zeros(Float32, maxiter)
-err6 = zeros(Float32, maxiter)
-for j=1:maxiter
-    set_params!(CH, θ+h*dθ)
-    Zx_, Zy_, _ = CH.forward(X+h*dX, Y+h*dY)
-    err5[j] = sqrt(norm(Zx_ - Zx)^2+norm(Zy_ - Zy)^2)
-    err6[j] = sqrt(norm(Zx_ - Zx - h*dZx)^2+norm(Zy_ - Zy - h*dZy)^2)
-    print(err5[j], "; ", err6[j], "\n")
-    global h = h/2f0
-end
-
-@test isapprox(err5[end] / (err5[1]/2^(maxiter-1)), 1f0; atol=1f1)
-@test isapprox(err6[end] / (err6[1]/4^(maxiter-1)), 1f0; atol=1f1)
-
-# Adjoint test
-
-set_params!(CH, θ)
-dZx, dZy, Zx, Zy, _, _ = CH.jacobian(dX, dY, dθ, X, Y)
-dZx_ = randn(Float32, size(dZx)); dZy_ = randn(Float32, size(dZy))
-dX_, dY_, dθ_, _, _, _ = CH.adjointJacobian(dZx_, dZy_, Zx, Zy)
-a = dot(dZx, dZx_)+dot(dZy, dZy_)
-b = dot(dX, dX_)+dot(dY, dY_)+dot(dθ, dθ_)
-@test isapprox(a, b; rtol=1f-1)  ####### need to check low accuracy here
+# Jacobian-related tests: 
+### NEED THESE
