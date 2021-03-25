686f51f47e1584e815f54019606ce7523ac337d2
rafaelorozco <rorozcom3>
2021-03-24 22:47:20 -0400

    gix gpu array problem in multiscale cond hint

--- src/utils/dimensionality_operations.jl
+++ src/utils/dimensionality_operations.jl
@@ -7,8 +7,8 @@ export general_squeeze, general_unsqueeze, squeeze, unsqueeze, wavelet_squeeze, 
 
 ####################################################################################################
 # General Squeeze and unsqueeze for user selection
-function general_squeeze(X::AbstractArray{T, N}; squeeze_type="normal", pattern="column") where {T, N}
-    if squeeze_type == "normal"
+function general_squeeze(X::AbstractArray{T, N}; squeeze_type="wavelet", pattern="column") where {T, N}
+    if squeeze_type == "shuffle"
         Y = squeeze(X; pattern=pattern)
     elseif squeeze_type == "wavelet"
         Y = wavelet_squeeze(X)
@@ -20,8 +20,8 @@ function general_squeeze(X::AbstractArray{T, N}; squeeze_type="normal", pattern=
     return Y
 end
 
-function general_unsqueeze(X::AbstractArray{T, N}; squeeze_type="normal", pattern="column") where {T, N}
-    if squeeze_type == "normal"
+function general_unsqueeze(X::AbstractArray{T, N}; squeeze_type="wavelet", pattern="column") where {T, N}
+    if squeeze_type == "shuffle"
         Y = unsqueeze(X; pattern=pattern)
     elseif squeeze_type == "wavelet"
         Y = wavelet_unsqueeze(X)
@@ -76,6 +76,7 @@ end
 
  See also: [`unsqueeze`](@ref), [`wavelet_squeeze`](@ref), [`wavelet_unsqueeze`](@ref)
 """
+#function squeeze(X::AbstractArray{T, N}, type::String="shuffle"; pattern="column") where {T, N}
 function squeeze(X::AbstractArray{T, N}; pattern="column") where {T, N}
 
     # Dimensions
@@ -132,6 +133,7 @@ end
 
  See also: [`squeeze`](@ref), [`wavelet_squeeze`](@ref), [`wavelet_unsqueeze`](@ref)
 """
+#function unsqueeze(Y::AbstractArray{T,N}, type::String="shuffle"; pattern="column") where {T, N}
 function unsqueeze(Y::AbstractArray{T,N}; pattern="column") where {T, N}
 
     # Dimensions
@@ -163,6 +165,10 @@ function unsqueeze(Y::AbstractArray{T,N}; pattern="column") where {T, N}
     return X
 end
 
+# function unsqueeze(X::AbstractArray{T,N}, Y::AbstractArray{T,4}, type::String="shuffle"; pattern="column") where {T,N}
+#     return unsqueeze(X,type; pattern=pattern), unsqueeze(Y, type; pattern=pattern)
+# end
+
 function unsqueeze(X::AbstractArray{T,N}, Y::AbstractArray{T,4}; pattern="column") where {T,N}
     return unsqueeze(X; pattern=pattern), unsqueeze(Y; pattern=pattern)
 end
@@ -171,7 +177,7 @@ end
 # Squeeze and unsqueeze using the wavelet transform
 
 """
-    Y = wavelet_squeeze(X; type=WT.db1)
+    Y = wavelet_squeeze(X,type=shuffle; type=WT.db1)
 
  Perform a 1-level channelwise 2D wavelet transform of X and squeeze output of each
  transform into 4 channels (per 1 input channel).
@@ -191,7 +197,9 @@ end
 
  See also: [`wavelet_unsqueeze`](@ref), [`squeeze`](@ref), [`unsqueeze`](@ref)
 """
+#function squeeze(X::AbstractArray{T, N}, type::String="wavelet"; wavelet_type=WT.db1) where {T, N}
 function wavelet_squeeze(X::AbstractArray{T, N}; type=WT.db1) where {T, N}
+  
     batchsize = size(X, N)
     N_in = size(X)[1:N-2]
     N_out = Tuple(nn÷2 for nn=size(X)[1:N-2])
@@ -203,7 +211,9 @@ function wavelet_squeeze(X::AbstractArray{T, N}; type=WT.db1) where {T, N}
     for i=1:batchsize
         for j=1:size(X, N-1)
             Ycurr = dwt(X[cinds..., j, i], wavelet(type), 1)
+            #Y[cinds..., (j-1)*nd + 1: j*nd, i] = squeeze(reshape(Ycurr, N_in..., 1, 1),"shuffle"; pattern="patch")
             Y[cinds..., (j-1)*nd + 1: j*nd, i] = squeeze(reshape(Ycurr, N_in..., 1, 1); pattern="patch")
+        
         end
     end
     return Y
