ea043a1da4ca248d15b76df29fcefd9fc7fb3845
Rafael <rao9787@gmail.com>
2022-05-21 17:34:23 -0400

    add residual block non square and galu

--- src/layers/layer_residual_block.jl
+++ src/layers/layer_residual_block.jl
@@ -64,7 +64,7 @@ struct ResidualBlock <: NeuralNetLayer
     W3::Parameter
     b1::Parameter
     b2::Parameter
-    fan::Bool
+    GALU::Bool
     strides
     pad
 end
@@ -75,22 +75,22 @@ end
 #  Constructors
 
 # Constructor
-function ResidualBlock(n_in, n_out, n_hidden; k1=3, k2=3, p1=1, p2=1, s1=1, s2=1, fan=false, ndims=2)
+function ResidualBlock(n_in, n_out,  n_hidden; k1=3, k2=3, p1=1, p2=1, s1=1, s2=1, GALU=false, ndims=2)
 
     k1 = Tuple(k1 for i=1:ndims)
     k2 = Tuple(k2 for i=1:ndims)
     # Initialize weights
     W1 = Parameter(glorot_uniform(k1..., n_in, n_hidden))
     W2 = Parameter(glorot_uniform(k2..., n_hidden, n_hidden))
-    W3 = Parameter(glorot_uniform(k1..., 2*n_out, n_hidden))
+    W3 = Parameter(glorot_uniform(k1..., n_out, n_hidden))
     b1 = Parameter(zeros(Float32, n_hidden))
     b2 = Parameter(zeros(Float32, n_hidden))
 
-    return ResidualBlock(W1, W2, W3, b1, b2, fan, (s1, s2), (p1, p2))
+    return ResidualBlock(W1, W2, W3, b1, b2, GALU, (s1, s2), (p1, p2))
 end
 
 # Constructor for given weights
-function ResidualBlock(W1, W2, W3, b1, b2; p1=1, p2=1, s1=1, s2=1, fan=false, ndims=2)
+function ResidualBlock(W1, W2, W3, b1, b2; p1=1, p2=1, s1=1, s2=1, GALU=false, ndims=2)
 
     # Make weights parameters
     W1 = Parameter(W1)
@@ -99,7 +99,7 @@ function ResidualBlock(W1, W2, W3, b1, b2; p1=1, p2=1, s1=1, s2=1, fan=false, nd
     b1 = Parameter(b1)
     b2 = Parameter(b2)
 
-    return ResidualBlock(W1, W2, W3, b1, b2, fan, (s1, s2), (p1, p2))
+    return ResidualBlock(W1, W2, W3, b1, b2, GALU, (s1, s2), (p1, p2))
 end
 
 ResidualBlock3D(args...; kw...) = ResidualBlock(args...; kw..., ndims=3)
@@ -108,52 +108,49 @@ ResidualBlock3D(args...; kw...) = ResidualBlock(args...; kw..., ndims=3)
 
 # Forward
 function forward(X1::AbstractArray{T, N}, RB::ResidualBlock; save=false) where {T, N}
-    inds =[i!=(N-1) ? 1 : (:) for i=1:N]
+    inds =[i!=(N-1) ? 1 : Colon() for i=1:N]
 
     Y1 = conv(X1, RB.W1.data; stride=RB.strides[1], pad=RB.pad[1]) .+ reshape(RB.b1.data, inds...)
-    X2 = ReLU(Y1)
+    X2 = LeakyReLU(Y1)
 
     Y2 = X2 + conv(X2, RB.W2.data; stride=RB.strides[2], pad=RB.pad[2]) .+ reshape(RB.b2.data, inds...)
-    X3 = ReLU(Y2)
+    X3 = LeakyReLU(Y2)
 
-    #cdims3 = DCDims(X1, RB.W3.data; nc=2*size(X1, N-1), stride=RB.strides[1], padding=RB.pad[1])
-    cdims3 = DCDims(X1, RB.W3.data; stride=RB.strides[1], padding=RB.pad[1])
+    cdims3 = DCDims(X1, RB.W3.data;  stride=RB.strides[1], padding=RB.pad[1])
     Y3 = ∇conv_data(X3, RB.W3.data, cdims3)
-    RB.fan == true ? (X4 = ReLU(Y3)) : (X4 = GaLU(Y3))
-
-    if save == false
-        return X4
-    else
-        return Y1, Y2, Y3, X2, X3
-    end
+    # Return if only recomputing state
+    save && (return Y1, Y2, Y3)
+    # Finish forward
+    RB.GALU == true ? (return GaLU(Y3)) : (return Y3) 
 end
 
 # Backward
 function backward(ΔX4::AbstractArray{T, N}, X1::AbstractArray{T, N},
                   RB::ResidualBlock; set_grad::Bool=true) where {T, N}
-    inds = [i!=(N-1) ? 1 : (:) for i=1:N]
+    inds = [i!=(N-1) ? 1 : Colon() for i=1:N]
     dims = collect(1:N-1); dims[end] +=1
 
     # Recompute forward states from input X
-    Y1, Y2, Y3, X2, X3 = forward(X1, RB; save=true)
+    Y1, Y2, Y3 = forward(X1, RB; save=true)
 
     # Cdims
-    cdims2 = DenseConvDims(X2, RB.W2.data; stride=RB.strides[2], padding=RB.pad[2])
-    cdims3 = DCDims(X1, RB.W3.data; stride=RB.strides[1], padding=RB.pad[1])
+    cdims2 = DenseConvDims(Y2, RB.W2.data; stride=RB.strides[2], padding=RB.pad[2])
+    cdims3 = DCDims(X1, RB.W3.data;  stride=RB.strides[1], padding=RB.pad[1])
 
     # Backpropagate residual ΔX4 and compute gradients
-    RB.fan == true ? (ΔY3 = ReLUgrad(ΔX4, Y3)) : (ΔY3 = GaLUgrad(ΔX4, Y3))
+    #RB.GALU == true ?  (ΔY3 = GaLUgrad(ΔX4, Y3)) : (ΔY3 = LeakyReLUgrad(ΔX4, Y3))
+    RB.GALU == true ?  (ΔY3 = GaLUgrad(ΔX4, Y3)) : (ΔY3 = ΔX4)
     ΔX3 = conv(ΔY3, RB.W3.data, cdims3)
-    ΔW3 = ∇conv_filter(ΔY3, X3, cdims3)
+    ΔW3 = ∇conv_filter(ΔY3, LeakyReLU(Y2), cdims3)
 
-    ΔY2 = ReLUgrad(ΔX3, Y2)
+    ΔY2 = LeakyReLUgrad(ΔX3, Y2)
     ΔX2 = ∇conv_data(ΔY2, RB.W2.data, cdims2) + ΔY2
-    ΔW2 = ∇conv_filter(X2, ΔY2, cdims2)
+    ΔW2 = ∇conv_filter(LeakyReLU(Y1), ΔY2, cdims2)
     Δb2 = sum(ΔY2, dims=dims)[inds...]
 
     cdims1 = DenseConvDims(X1, RB.W1.data; stride=RB.strides[1], padding=RB.pad[1])
 
-    ΔY1 = ReLUgrad(ΔX2, Y1)
+    ΔY1 = LeakyReLUgrad(ΔX2, Y1)
     ΔX1 = ∇conv_data(ΔY1, RB.W1.data, cdims1)
     ΔW1 = ∇conv_filter(X1, ΔY1, cdims1)
     Δb1 = sum(ΔY1, dims=dims)[inds...]
@@ -175,7 +172,7 @@ end
 ## Jacobian-related functions
 function jacobian(ΔX1::AbstractArray{T, N}, Δθ::Array{Parameter, 1},
                   X1::AbstractArray{T, N}, RB::ResidualBlock) where {T, N}
-    inds = [i!=(N-1) ? 1 : (:) for i=1:N]
+    inds = [i!=(N-1) ? 1 : Colon() for i=1:N]
     # Cdims
     cdims1 = DenseConvDims(X1, RB.W1.data; stride=RB.strides[1], padding=RB.pad[1])
 
@@ -209,15 +206,3 @@ end
 function adjointJacobian(ΔX4::AbstractArray{T, N}, X1::AbstractArray{T, N}, RB::ResidualBlock) where {T, N}
     return backward(ΔX4, X1, RB; set_grad=false)
 end
-
-## Other utils
-# Clear gradients
-function clear_grad!(RB::ResidualBlock)
-    RB.W1.grad = nothing
-    RB.W2.grad = nothing
-    RB.W3.grad = nothing
-    RB.b1.grad = nothing
-    RB.b2.grad = nothing
-end
-
-get_params(RB::ResidualBlock) = [RB.W1, RB.W2, RB.W3, RB.b1, RB.b2]
